6e8adb3da2782124f06ffdd0804f8313aea39d10
Md.Azmat Ullah Kuarisi <97841891+mdazmat01@users.noreply.github.com>
2024-08-24 14:48:38 +0600

    Testimonial_added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+// Filter
 $(function($) {
     $.autofilter({
         animation:true,
         duration: 0.5,
+    });
+  });
+
+
+//   Slick Slider
+  $(document).ready(function(){
+    $('.client').slick({
+        autoplay:true,
+        autoplaySpeed: 3000,
+        dots:true,
+        arrows:true,
+        slidesToShow: 2
     });
   });
