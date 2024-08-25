599f121d8b5fc3a14087dc9d0686e95457a6c29d
Md.Azmat Ullah Kuarisi <97841891+mdazmat01@users.noreply.github.com>
2024-08-25 11:55:24 +0600

    bg_changed

--- js/main.js
+++ js/main.js
@@ -8,12 +8,22 @@ $(function($) {
 
 
 //   Slick Slider
-  $(document).ready(function(){
-    $('.client').slick({
-        autoplay:true,
-        autoplaySpeed: 3000,
-        dots:true,
-        arrows:true,
-        slidesToShow: 2
-    });
+$(document).ready(function(){
+  $('.client').slick({
+      autoplay:true,
+      autoplaySpeed: 3000,
+      dots:true,
+      arrows:true,
+      slidesToShow: 1
   });
+});
+
+$(document).ready(function(){
+  $('.team').slick({
+      autoplay:true,
+      autoplaySpeed: 3000,
+      dots:true,
+      arrows:true,
+      slidesToShow: 3
+  });
+});
